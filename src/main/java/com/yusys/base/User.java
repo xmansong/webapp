8a95c2c13432110bb47c3752aeb8832260d5e14a
dusong <dusong@yusys.com.cn>
2018-01-25 15:10:54 +0800

    cc

--- src/main/java/com/yusys/base/User.java
+++ src/main/java/com/yusys/base/User.java
@@ -9,6 +9,6 @@ public class User {
  private String id5;
  private String id4;
 
- private String id6abc; //211111222211111xxxx
- private String xx; //211222222211111111000000000
+ private String id6abc; //abc
+ private String xx; //abc
 }
